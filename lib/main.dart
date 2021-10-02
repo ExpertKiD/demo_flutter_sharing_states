42d3c81d6ee736bc3085b72e870a4d3196670b0f
Saurav Adhikari <sauravads123@gmail.com>
2021-10-02 16:02:07 +0545

    Add scoped model to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'inherited_widget_demo.dart';
+import 'scoped_model_demo.dart';
 
 /// For InheritedWidgets demo use
-main() => runApp(const MyInheritedWidgetDemoApp());
+// main() => runApp(const MyInheritedWidgetDemoApp());
+
+/// For Scoped Model demo use
+main() => runApp(const MyScopedModelDemoApp());
---
+++ lib/scoped_model_demo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyScopedModelDemoApp extends StatelessWidget {
+  const MyScopedModelDemoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
